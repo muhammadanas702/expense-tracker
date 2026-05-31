fae879dd6519fc242d3a3a9fc3334cd7e38cc84e
Muhammad Anas <anasali2988@gmail.com>
2026-06-01 04:04:30 +0500

    fix db switching local and live

---
+++ config/db.live.php
@@ -0,0 +1,8 @@
+<?php
+$host = "sql103.infinityfree.com";
+$dbname = "if0_41994702_expenseflow";
+$username = "if0_41994702";
+$password = "Anasali001";
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- config/db.php
+++ config/db.php
@@ -1,34 +1,7 @@
 <?php
 
-if (
-    $_SERVER['HTTP_HOST'] == 'localhost'
-    || $_SERVER['HTTP_HOST'] == '127.0.0.1'
-) {
-
-    $host = "localhost";
-    $dbname = "expense_tracker";
-    $username = "root";
-    $password = "ipc@umt";
-
+if (file_exists(__DIR__ . "/db.local.php")) {
+    require_once "db.local.php";
 } else {
-
-    $host = "sql103.infinityfree.com";
-    $dbname = "if0_41994702_expenseflow";
-    $username = "if0_41994702";
-    $password = "Anasali001";
-}
-
-try {
-
-    $conn = new PDO(
-        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
-        $username,
-        $password
-    );
-
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch (PDOException $e) {
-
-    die("Connection failed: " . $e->getMessage());
+    require_once "db.live.php";
 }
