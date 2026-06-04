dbfe7698ad2d279313b48771e06cc2271c86bdd1
Muhammad Anas <anasali2988@gmail.com>
2026-06-05 04:04:01 +0500

    fix dashboard favicon

--- dashboard.php
+++ dashboard.php
@@ -153,7 +153,7 @@ if (!empty($categoryTotals)) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="icon" type="image/png" href="/expense-tracker/favicon.png">
+    <link rel="icon" type="image/png" href="/favicon.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     <title>ExpenseFlow | Dashboard</title>
